061111de19ae79a4391da20ebd0a254d7c2e70c9
paranoiachains <bluntsside@gmail.com>
2026-03-04 12:12:35 +0300

    feat: use all tresitter features

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,15 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		tag = "v0.10.0",
 		build = ":TSUpdate",
 		event = "BufReadPost",
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
+		},
 		config = function()
-			require("nvim-treesitter.config").setup({
+			require("nvim-treesitter.configs").setup({
 				ensure_installed = {
 					"lua",
 					"rust",
@@ -15,7 +20,7 @@ return {
 					"html",
 					"c",
 					"markdown",
-					"js",
+					"javascript",
 					"python",
 				},
 				auto_install = true,
@@ -23,6 +28,49 @@ return {
 				indent = { enable = true },
 				incremental_selection = {
 					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<CR>",
+						scope_incremental = "<S-CR>",
+						node_decremental = "<BS>",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["ab"] = "@block.outer",
+							["ib"] = "@block.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]c"] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[c"] = "@class.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
 				},
 			})
 		end,
