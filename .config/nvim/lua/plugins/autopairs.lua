d365dcbf9406586e2319b4ea2662ea0aa6d5ba6c
paranoiachains <bluntsside@gmail.com>
2026-03-08 00:59:31 +0300

    feat: use mini.pairs cause its just smaller

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,20 +1,3 @@
 return {
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			local autopairs = require("nvim-autopairs")
-			autopairs.setup({
-				check_ts = true,
-				fast_wrap = {},
-				disable_filetype = { "TelescopePrompt", "vim" },
-			})
-
-			local cmp_status, cmp = pcall(require, "cmp")
-			if cmp_status then
-				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-			end
-		end,
-	},
+	{ "nvim-mini/mini.pairs", version = false, opts = {} },
 }
