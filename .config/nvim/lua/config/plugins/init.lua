e9b99d50bb4e705359915489e6d9d200c0f8e2f3
keira <keira.inbox@proton.me>
2026-07-09 21:58:22 +0300

    chore: weird commit

--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -1,37 +1,35 @@
 local gh = function(repo)
-	return "https://github.com/" .. repo
+    return "https://github.com/" .. repo
 end
 
 vim.pack.add({
-	gh("saghen/blink.cmp"),
-	gh("saghen/blink.lib"),
+    gh("saghen/blink.cmp"),
+    gh("saghen/blink.lib"),
 
-	gh("mason-org/mason.nvim"),
-	gh("mason-org/mason-lspconfig.nvim"),
+    gh("mason-org/mason.nvim"),
+    gh("mason-org/mason-lspconfig.nvim"),
 
-	gh("stevearc/conform.nvim"),
+    gh("stevearc/conform.nvim"),
 
-	gh("nvim-lualine/lualine.nvim"),
-	gh("folke/tokyonight.nvim"),
+    gh("folke/tokyonight.nvim"),
+    gh("nvim-treesitter/nvim-treesitter"),
 
-	gh("akinsho/toggleterm.nvim"),
+    gh("akinsho/toggleterm.nvim"),
 
-	gh("nvim-telescope/telescope.nvim"),
-	gh("nvim-lua/plenary.nvim"),
-	gh("nvim-telescope/telescope-fzf-native.nvim"),
-	gh("nvim-telescope/telescope-file-browser.nvim"),
+    gh("nvim-telescope/telescope.nvim"),
+    gh("nvim-lua/plenary.nvim"),
+    gh("nvim-telescope/telescope-fzf-native.nvim"),
+    gh("nvim-telescope/telescope-file-browser.nvim"),
 
-	gh("nvim-mini/mini.pairs"),
-	gh("nvim-mini/mini.surround"),
+    gh("nvim-mini/mini.pairs"),
+    gh("nvim-mini/mini.surround"),
 })
 
 require("config.plugins.mason")
 require("config.plugins.format")
-require("config.plugins.lualine")
 require("config.plugins.completion")
 require("config.plugins.theme")
 require("config.plugins.terminal")
 require("config.plugins.telescope")
 require("config.plugins.mini")
-require("config.plugins.treesitter").setup({ "rust", "markdown", "markdown_inline" })
--- require("config.plugins.local")
+require("config.plugins.treesitter")
